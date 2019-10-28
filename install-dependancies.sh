7f3d862354f6c8eb22bdf27046f2bc7e83e097cd
Chabam <fchabot1337@gmail.com>
2019-10-28 10:22:22 -0400

    Ajout de clang dans les dépendances

--- install-dependancies.sh
+++ install-dependancies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 INSTALL_CMD="apt install"
-sudo $INSTALL_CMD libsndfile1 libsndfile1-dev portaudio19-dev libpulse-dev libncurses5-dev
+sudo $INSTALL_CMD libsndfile1 libsndfile1-dev portaudio19-dev libpulse-dev libncurses5-dev clang
